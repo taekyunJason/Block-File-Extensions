b35d9083ae6e357c3f33540e62baa4e18c60d2a5
jason taekyun Kim <saurus234@gmail.com>
2022-07-05 18:31:25 +0900

    update

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const port = 3000;
 
@@ -6,6 +7,12 @@ const requestMiddleWare = (req, res, next) => {
   console.log("request URL : ", req.originalUrl, "-", new Date());
   next();
 };
+
+app.use(morgan("dev"));
+//app.use("/", express.static(__dirname, "static"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(requestMiddleWare);
 
 app.get("/", (req, res) => {
   console.log("메인 화면입니다");
